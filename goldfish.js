7987cb6453b0dae1552f90db4f1c29ab53e3ba37
Ginny Hung <pen@ofaginny.com>
2012-02-05 21:35:54 -0500

    post

--- goldfish.js
+++ goldfish.js
@@ -3,11 +3,16 @@ $(document).ready(function(){
 			var boxy = document.createElement("div");
 			$('body').append(boxy);
 			$(boxy).attr("id","boxy");
-			$(boxy).load("insertHTML.html");
+			$(boxy).load("insertHTML.html",posting);
 			var URLz = document.location.href;
 			console.log(URLz);
 			var titlez = $('title').html();
 			console.log(titlez);		
-	});
+		});
+	
+	function posting(){
+		$('#submit').click(function(){
+			$.post("/send",{address:"niniu", subject:"sdfsdf", body:"sdfasdf"});
+		});
+	};
 });
-
